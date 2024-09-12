f072b4775c97b7789ed2a862dcb4aa0a134cc0ab
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2024-09-12 16:50:01 -0300

    fix: export configs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { rule } from './rules/no-date-equality-check';
+import * as configs from './configs/';
+import { rules } from './rules/';
 
 const packageData = require('../package.json') as Record<string, string>;
 const meta = { name: packageData.name, version: packageData.version };
-const rules = {
-  'no-date-equality-check': rule,
-};
 
-export { meta, rules };
+export { configs, meta, rules };
---
+++ src/rules/index.ts
@@ -0,0 +1,5 @@
+import { rule } from './no-date-equality-check';
+
+export const rules = {
+  'no-date-equality-check': rule,
+};
